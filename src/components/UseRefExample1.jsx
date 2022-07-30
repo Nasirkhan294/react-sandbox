dfd79879fc5d0e505eefc2b678b03a87e8f4d7eb
Nasirkhan294 <nasirbhatti.nk@gmail.com>
2022-07-30 13:16:37 +0500

    First useRef Example

--- src/components/UseRefExample1.jsx
+++ src/components/UseRefExample1.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { useRef } from 'react'
 
 function UseRefExample1() {
+    const inputRef = useRef();
+    const paraRef = useRef();
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log(inputRef.current.value);
+        inputRef.current.value = "Hello"
+        inputRef.current.style.backgroundColor = 'green'
+        paraRef.current.innerText = "See you sooon!"
+
+    }
     return (
-        <div >UseRefExample1</div>
+        <div>
+            <form onSubmit={onSubmit}>
+                <label htmlFor="name">Name</label>
+                <input type="text" id='name' ref={inputRef} className='form-control mb-2' />
+                <button type='submit' className='btn btn-primary'>Submit</button>
+                <p onClick={() => inputRef.current.focus()} ref={paraRef}>Hello</p>
+            </form>
+        </div>
     )
 }
 
